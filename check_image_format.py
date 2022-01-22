7b482b3ca41775459364827b153448f12a0ee9a6
Nitin Tiwari <37960279+NSTiwari@users.noreply.github.com>
2022-01-22 21:07:14 +0530

    Check image format.

--- check_image_format.py
+++ check_image_format.py
@@ -1,4 +1,19 @@
 import PIL.Image
+import glob
+import os
 
-img = PIL.Image.open('cartoon57.jpg')
-print(img.format)
+images = [i for i in glob.glob("./dataset/*.jpg")]
+
+jpeg = 0
+others = 0
+
+for i in images:
+	img = PIL.Image.open(i)
+	if img.format == 'JPEG':
+		jpeg += 1
+	else:
+		print('{} is {}'.format(i, img.format))
+		others +=1
+
+print("Total JPEG images found: ", jpeg)
+print("Total other images found: ", others) 
